b4dc2957032d1030fe0fb06ebf95a8947f940c0c
Robsdedude <rouven.bauer@neo4j.com>
2023-04-27 17:20:12 +0200

    Update python example to use execute_query
    Using the new simpler API introduced in drivers 5.5 and stabilized in 5.8

--- code/python/example.py
+++ code/python/example.py
@@ -3,19 +3,17 @@
 
 from neo4j import GraphDatabase, basic_auth
 
-driver = GraphDatabase.driver(
-  "neo4j+s://demo.neo4jlabs.com:7687",
-  auth=basic_auth("mUser", "s3cr3t"))
-
 cypher_query = '''
 MATCH (m:Movie {title:$movieTitle})<-[:ACTED_IN]-(a:Person) RETURN a.name as actorName
 '''
 
-with driver.session(database="movies") as session:
-  results = session.read_transaction(
-    lambda tx: tx.run(cypher_query,
-                      movieTitle="The Matrix").data())
-  for record in results:
-    print(record['actorName'])
-
-driver.close()
+with GraphDatabase.driver(
+    "neo4j+s://demo.neo4jlabs.com:7687",
+    auth=("mUser", "s3cr3t")
+) as driver:
+    result = driver.execute_query(
+        cypher_query,
+        movieTitle="The Matrix",
+        database_="movies")
+    for record in result.records:
+        print(record['actorName'])
