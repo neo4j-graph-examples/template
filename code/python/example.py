c819359441e1ce90878492177e097cebd297e5e7
Robsdedude <rouven.bauer@neo4j.com>
2026-04-27 15:12:34 +0200

    Remove unused import from Python example (#41)

--- code/python/example.py
+++ code/python/example.py
@@ -1,7 +1,7 @@
 # pip3 install neo4j
 # python3 example.py
 
-from neo4j import GraphDatabase, basic_auth
+from neo4j import GraphDatabase
 
 cypher_query = '''
 MATCH (m:Movie {title:$movieTitle})<-[:ACTED_IN]-(a:Person) RETURN a.name as actorName
